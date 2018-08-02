3ba1379c9c459fa75b0238af5791509603da3f74
dnyt <lyricaldnyt@gmail.com>
2018-08-02 15:45:56 +0300

    Create Food and Category model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200,db_index=True)
+
+    def __str__(self):
+        return self.name
+
+class Food(models.Model):
+    category = models.ForeignKey(Category,related_name='foods')
+    name = models.CharField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='foods/%Y/%m/%d',blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'price'))
+
+    def __str__(self):
+        return self.name
