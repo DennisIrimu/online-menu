4c4bc5fb7e8b4c2f46c310fc5306d3efe683575f
dnyt <lyricaldnyt@gmail.com>
2018-08-03 11:17:08 +0300

    Registered models in admin panel, Created env file to store variables

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Category,Food
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+admin.site.register(Category, CategoryAdmin)
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'available']
+    list_filter = ['available']
+    list_editable = ['price', 'available']
+admin.site.register(Product, ProductAdmin)
